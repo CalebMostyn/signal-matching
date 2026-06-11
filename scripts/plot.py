f44b374bcb8156390dfdbfdf31f837b363ff8c75
CalebMostyn <calebmostyn@gmail.com>
2026-06-10 20:51:54 -0500

    Plotting All Reference Data

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,10 +1,22 @@
 import matplotlib
 matplotlib.use('TkAgg')
 
+import os
 from matplotlib import pyplot as plt
 from signal_matcher.signal import Signal
 
-ref1 = Signal('data/reference/ref_signal0.csv')
+# Plot all the reference signals on a single graph
+ref_signals: list[Signal] = []
+with os.scandir('data/reference/') as files:
+    for file in files:
+        if file.is_file():
+            ref_signals.append(Signal(file.path))
 
-plt.plot(ref1.time, ref1.intensity)
+for signal in ref_signals:
+    plt.plot(signal.time, signal.intensity, label=signal.name)
+
+plt.legend()
+plt.xlabel("Time")
+plt.ylabel("Intensity")
+plt.title("Reference Signals")
 plt.show()
--- src/signal_matcher/signal.py
+++ src/signal_matcher/signal.py
@@ -1,8 +1,11 @@
+import os
 import numpy as np
 
 class Signal():
     time: np.array
     intensity: np.array
+    name: string
 
-    def __init__(self, filename: str):
-        self.time, self.intensity = np.loadtxt(filename, delimiter=",", skiprows=1, unpack=True)
+    def __init__(self, filepath: str) -> None:
+        self.name = os.path.basename(filepath)
+        self.time, self.intensity = np.loadtxt(filepath, delimiter=",", skiprows=1, unpack=True)

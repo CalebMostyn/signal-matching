fbac0f87a4c3c58385e5271f71081df1f82606b8
CalebMostyn <calebmostyn@gmail.com>
2026-06-10 22:20:37 -0500

    First Pass at Brute Force Algorithm

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,17 +1,18 @@
+import os
+
 import matplotlib
 matplotlib.use('TkAgg')
-
-import os
 from matplotlib import pyplot as plt
 from signal_matcher.signal import Signal
 
-# Plot all the reference signals on a single graph
+# Load reference signals
 ref_signals: list[Signal] = []
 with os.scandir('data/reference/') as files:
     for file in files:
         if file.is_file():
-            ref_signals.append(Signal(file.path))
+            ref_signals.append(Signal(filepath=file.path))
 
+# Plot all the reference signals on a single graph
 for signal in ref_signals:
     plt.plot(signal.time, signal.intensity, label=signal.name)
 
--- src/signal_matcher/signal.py
+++ src/signal_matcher/signal.py
@@ -2,10 +2,17 @@ import os
 import numpy as np
 
 class Signal():
-    time: np.array
-    intensity: np.array
-    name: string
+    time: np.array = None
+    intensity: np.array = None
+    name: string = ""
 
-    def __init__(self, filepath: str) -> None:
-        self.name = os.path.basename(filepath)
-        self.time, self.intensity = np.loadtxt(filepath, delimiter=",", skiprows=1, unpack=True)
+    def __init__(self, filepath: str = "", time: np.array = None, intensity: np.array = None) -> None:
+        if filepath != "":
+            self.name = os.path.basename(filepath)
+            self.time, self.intensity = np.loadtxt(filepath, delimiter=",", skiprows=1, unpack=True)
+        else:
+            if not time is None:
+                self.time = time
+            if not intensity is None:
+                self.intensity = intensity
+

d3e41b555d0d81909ad458b7ca08f9d11efe49f6
CalebMostyn <calebmostyn@gmail.com>
2026-06-11 22:52:55 -0500

    Cleaning Up Cross Correlation Script

--- scripts/cross_correlation.py
+++ scripts/cross_correlation.py
@@ -6,17 +6,13 @@ if __name__ == "__main__":
 
     solver: Solver = Solver('data/experimental', 'data/reference')
 
-    results: ResultSet = solver.cross_correlation_solver(Signal.nrmse, corr_method='direct')
-    # generate csv from results
-    results.to_csv(f'results/cross_correlation_sliding.csv')
-    # generate plots from each result
-    for sample, result in results.data.items():
-        result.plot(sample, f'plots/cross_correlation_sliding/{sample.name}.png')
-
-    results: ResultSet = solver.cross_correlation_solver(Signal.nrmse, corr_method='fft')
-    # generate csv from results
-    results.to_csv(f'results/cross_correlation_fft.csv')
-    # generate plots from each result
-    for sample, result in results.data.items():
-        result.plot(sample, f'plots/cross_correlation_fft/{sample.name}.png')
+    # use both sliding time-domain method and frequency domain method
+    corr_methods: list[str] = ['direct', 'fft']
+    for corr_method in corr_methods:
+        results: ResultSet = solver.cross_correlation_solver(Signal.nrmse, corr_method=corr_method)
+        # generate csv from results
+        results.to_csv(f'results/cross_correlation_{corr_method}.csv')
+        # generate plots from each result
+        for sample, result in results.data.items():
+            result.plot(sample, f'plots/cross_correlation_{corr_method}/{sample.name}.png')
 
